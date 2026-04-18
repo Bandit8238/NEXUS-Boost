2ae4c1300048a3ee1e09c65d0a909e939a5e0d4b
Bandit8238 <zech.sq@gmail.com>
2026-04-17 23:35:00 -0600

    processBoost.js

--- core/optimizer/processBoost.js
+++ core/optimizer/processBoost.js
@@ -1,1 +1,7 @@
+const { exec } = require("child_process");
 
+function boostFortnite() {
+    exec(`wmic process where name="FortniteClient-Win64-Shipping.exe" CALL setpriority 128`);
+}
+
+module.exports = { boostFortnite };
