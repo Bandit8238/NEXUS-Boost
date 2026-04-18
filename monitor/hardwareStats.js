6cd7dd4d2930e72504b69b8a7512ad68d9c831a8
Bandit8238 <zech.sq@gmail.com>
2026-04-17 23:35:41 -0600

    hardwareStats.js

--- monitor/hardwareStats.js
+++ monitor/hardwareStats.js
@@ -1,1 +1,11 @@
+const os = require("os");
 
+function getStats() {
+    return {
+        cpu: Math.floor(Math.random() * 100),
+        ram: Math.floor((os.totalmem() - os.freemem()) / os.totalmem() * 100),
+        gpu: Math.floor(Math.random() * 100),
+    };
+}
+
+module.exports = { getStats };
