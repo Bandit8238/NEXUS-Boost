1fca788123d8a84b3a223e69535b289106c9dd69
Bandit8238 <zech.sq@gmail.com>
2026-04-17 23:30:53 -0600

    preload.js

--- app/preload.js
+++ app/preload.js
@@ -1,1 +1,6 @@
+const { contextBridge, ipcRenderer } = require("electron");
 
+contextBridge.exposeInMainWorld("api", {
+    boost: () => ipcRenderer.invoke("boost"),
+    getStats: () => ipcRenderer.invoke("stats"),
+});
