5faebf09a5bcb1c357ede1de058e6c0eb8c7e5e0
Bandit8238 <zech.sq@gmail.com>
2026-04-17 23:32:18 -0600

    ui.js
    Added functionality to boost system performance and display fake live stats.

--- app/renderer/ui.js
+++ app/renderer/ui.js
@@ -1,1 +1,14 @@
+const boostBtn = document.getElementById("boostBtn");
 
+boostBtn.onclick = async () => {
+    await window.api.boost();
+    alert("BOOST MODE ACTIVATED 🚀");
+};
+
+// fake live stats (replace later with real monitoring)
+setInterval(() => {
+    document.getElementById("cpu").innerText = Math.floor(Math.random() * 80);
+    document.getElementById("ram").innerText = Math.floor(Math.random() * 80);
+    document.getElementById("gpu").innerText = Math.floor(Math.random() * 90);
+    document.getElementById("fps").innerText = 120 + Math.floor(Math.random() * 80);
+}, 1000);
