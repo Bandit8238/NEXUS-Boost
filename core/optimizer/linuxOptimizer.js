492137eec16819d72d176e0bc133cc314ea6c373
Bandit8238 <zech.sq@gmail.com>
2026-04-17 23:34:36 -0600

    linuxOptimizer.js

--- core/optimizer/linuxOptimizer.js
+++ core/optimizer/linuxOptimizer.js
@@ -1,1 +1,7 @@
+const { exec } = require("child_process");
 
+function performanceMode() {
+    exec("echo 'performance' | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor");
+}
+
+module.exports = { performanceMode };
