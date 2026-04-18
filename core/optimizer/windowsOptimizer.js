d81a42be39ff3f69b1df7532c354759dffe4df09
Bandit8238 <zech.sq@gmail.com>
2026-04-17 23:34:00 -0600

    windowsOptimizer.js

--- core/optimizer/windowsOptimizer.js
+++ core/optimizer/windowsOptimizer.js
@@ -1,1 +1,14 @@
+const { exec } = require("child_process");
 
+function enableHighPerformanceMode() {
+    exec("powercfg -setactive SCHEME_MIN");
+}
+
+function disableBackgroundApps() {
+    exec("powershell -Command \"Get-Process | Where-Object {$_.CPU -gt 50} | Stop-Process -Force\"");
+}
+
+module.exports = {
+    enableHighPerformanceMode,
+    disableBackgroundApps
+};
